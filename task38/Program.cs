c3101a1f24479e2aee27cd4040cc2f377fb62019
Rada <rada.bikreva@mail.ru>
2023-08-01 21:23:48 +0500

    Added task38

---
+++ task38/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+int size = 10;
+int[] arr = new int[size];
+FillArrayRandomNumbers(arr);
+PrintArray(arr);
+
+int max = arr[0];
+int min = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
